478c416b7963e05c7b91171cf4a5c3c8f36a1e0a
Yevgeniy <zleonenkov@gmail.com>
2019-07-06 17:41:39 +0300

    Updates

---
+++ precache-manifest.34008bae075ffe5cf44c928ec0b80da7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b33e9b4bbeea8ca0f3dc305fa6b427",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "13fcfad03c811345ee4c",
+    "url": "/react_dynamic-list-of-todos/static/css/main.d1caa26f.chunk.css"
+  },
+  {
+    "revision": "cda7ee185f4ec272927e",
+    "url": "/react_dynamic-list-of-todos/static/js/2.96df1c6b.chunk.js"
+  },
+  {
+    "revision": "13fcfad03c811345ee4c",
+    "url": "/react_dynamic-list-of-todos/static/js/main.e4e84bef.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
